b4668d649ac8bf53dd7574a698526c755d50591d
陳羿豐 Yi-Feng Chen <randomfrank8@gmail.com>
2022-01-03 00:10:10 +0800

    jianpu search box

--- routes/jianpu.js
+++ routes/jianpu.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('jianpu', { place: 'jianpu' });
 });
 
+router.get('/search', function(req, res, next) {
+  res.render('jianpuSearch', { place: 'jianpu', query: req.query.jianpu });
+});
+
 module.exports = router;
