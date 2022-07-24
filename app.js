00987563fceadff5ba4997f8058ce0e0a484de6d
陳羿豐 Yi-Feng Chen <randomfrank8@gmail.com>
2022-07-24 17:00:37 +0800

    set wasm mime type

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// set MIME type
+express.static.mime.define({'application/wasm': ['wasm']});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
