e7e018ec9678bf7d5816bbd365cd325d7dd19ef9
陳羿豐 Yi-Feng Chen <randomfrank8@gmail.com>
2022-08-15 00:01:57 +0800

    show number of songs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+const { querySQL } = require('../connectDB');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { place: 'main' });
+router.get('/', async function(req, res, next) {
+  try {
+    var cnt = await querySQL('select count(id) as num from songs');
+    res.render('index', { place: 'main', numSongs: cnt[0].num });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
