6645cbc569870c739ccacc54db6fdc661af8f020
陳羿豐 Yi-Feng Chen <randomfrank8@gmail.com>
2022-08-22 23:56:33 +0800

    attempt to sync db

--- connectDB.js
+++ connectDB.js
@@ -57,9 +57,11 @@ conn.query(
 );
 
 let jianpuDB = {};
+let lastDate = new Date(0);
+let firstTimeInit = true;
 
 function initDB() {
-    return querySQL('SELECT * FROM songs', []).then(results => {
+    return querySQL('SELECT * FROM songs WHERE modify_time >= ? - INTERVAL 30 SECOND', [lastDate]).then(results => {
         results.forEach(row => {
             let song = {
                 name: row.name,
@@ -67,6 +69,8 @@ function initDB() {
                 singer: row.singer,
                 language: row.language,
                 jianpu: row.jianpu,
+                creation_time: row.creation_time,
+                modify_time: row.modify_time,
             };
             if (row.jianpu) {
                 let {pitch, duration} = jianpu_to_pitch(row.jianpu);
@@ -74,8 +78,15 @@ function initDB() {
                 song.duration = duration;
             }
             jianpuDB[row.id] = song;
-            console.log(row.name);
+            if (song.modify_time > lastDate) {
+                lastDate = song.modify_time;
+            }
         });
+        if (firstTimeInit) {
+            console.log('[initDB] found %d songs in sql', results.length);
+            firstTimeInit = false;
+        }
+        setTimeout(initDB, Math.random() * 30 + 30);
     });
 }
 
