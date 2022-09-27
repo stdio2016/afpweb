d886887d2e89f834cd03b335b776fedae77c8c69
陳羿豐 Yi-Feng Chen <randomfrank8@gmail.com>
2022-09-27 23:22:18 +0800

    todo list add

--- routes/todo.js
+++ routes/todo.js
@@ -8,4 +8,23 @@ router.get('/', async function(req, res, next) {
   return res.render('todo', { todos });
 });
 
+router.get('/add', async function(req, res, next) {
+  return res.render('todoAdd', {  });
+});
+router.post('/add', async function(req, res, next) {
+  var body = req.body || {};
+  var pass = body.password;
+  if (pass !== process.env.PAW) {
+    return res.redirect('../TODO/add');
+  }
+
+  var table = (await db).collection('todo');
+  await table.insertOne({
+    title: body.title + '',
+    description: body.description + '',
+    status: body.status + '',
+  });
+  return res.redirect('../TODO');
+});
+
 module.exports = router;
