cd4e12872da4d675712c210058dacc658e0f11ba
陳羿豐 Yi-Feng Chen <randomfrank8@gmail.com>
2022-09-30 23:12:22 +0800

    todo edit

--- routes/todo.js
+++ routes/todo.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const createHttpError = require('http-errors');
+const { ObjectId } = require('mongodb');
 const { db } = require('../mongo/mongoConnect');
 var router = express.Router();
 
@@ -25,6 +27,41 @@ router.post('/add', async function(req, res, next) {
     status: body.status + '',
   });
   return res.redirect('../TODO');
+});
+
+router.get('/edit', async function(req, res, next) {
+  try {
+    var table = (await db).collection('todo');
+    var item = await table.findOne({ _id: new ObjectId(req.query.id) });
+    if (!item) {
+      return next(createHttpError(404));
+    }
+    return res.render('todoEdit', { item } );
+  } catch (err) {
+    return next(err);
+  }
+});
+router.post('/edit', async function(req, res, next) {
+  try {
+    var body = req.body || {};
+    var pass = body.password;
+    if (pass !== process.env.PAW) {
+      return res.redirect('../TODO/edit?id=' + req.query.id);
+    }
+
+    var table = (await db).collection('todo');
+    var { title, description, status } = req.query;
+    await table.updateOne({ _id: new ObjectId(req.query.id) }, {
+      $set: {
+        title: title + '',
+        description: description + '',
+        status: status + '',
+      }
+    });
+    return res.redirect('../TODO');
+  } catch (err) {
+    return next(err);
+  }
 });
 
 module.exports = router;
