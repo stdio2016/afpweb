6e4b001bf701d0bf161539863b5a642f81aa82e3
陳羿豐 Yi-Feng Chen <randomfrank8@gmail.com>
2022-12-21 23:58:25 +0800

    fix todo list

--- routes/todo.js
+++ routes/todo.js
@@ -7,6 +7,13 @@ var router = express.Router();
 router.get('/', async function(req, res, next) {
   var table = (await db).collection('todo');
   var todos = await table.find().toArray();
+  var order = {Open: 0, Committed: 1, Wait: 2, Done: 3, Abandoned: 4};
+  todos.sort((a, b) => {
+    if (order[a.status] === order[b.status]) {
+      return a.modifyDate - b.modifyDate;
+    }
+    return order[a.status] - order[b.status];
+  });
   return res.render('todo', { todos });
 });
 
@@ -25,6 +32,7 @@ router.post('/add', async function(req, res, next) {
     title: body.title + '',
     description: body.description + '',
     status: body.status + '',
+    modifyDate: new Date(),
   });
   return res.redirect('../TODO');
 });
@@ -50,12 +58,13 @@ router.post('/edit', async function(req, res, next) {
     }
 
     var table = (await db).collection('todo');
-    var { title, description, status } = req.query;
+    var { title, description, status } = req.body;
     await table.updateOne({ _id: new ObjectId(req.query.id) }, {
       $set: {
         title: title + '',
         description: description + '',
         status: status + '',
+        modifyDate: new Date(),
       }
     });
     return res.redirect('../TODO');
