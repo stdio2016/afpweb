74dbc54ab8d2c3a81d33509a3b494bf99d7e39b5
陳羿豐 Yi-Feng Chen <randomfrank8@gmail.com>
2023-12-08 00:56:13 +0800

    change play pitch timbre

--- public/javascripts/pastQueriesQbsh.js
+++ public/javascripts/pastQueriesQbsh.js
@@ -1,6 +1,10 @@
 function playPitch() {
+    var timbreRe = new Float32Array(21);
+    var timbreIm = new Float32Array(21);
+    for (var i = 1; i <= 20; i++) timbreIm[i] = Math.exp((i-1) * -0.25);
+    var timbre = actx.createPeriodicWave(timbreRe, timbreIm);
     var osc = actx.createOscillator();
-    osc.type ="square";
+    osc.setPeriodicWave(timbre);
     var ga = actx.createGain();
     ga.gain.value = 0.3;
     osc.connect(ga);
